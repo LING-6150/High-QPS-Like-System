7f6bd0dc4fb55c23374d26b8f2e406e7e588275d
LING-6150 <duan.lin@northeastern.edu>
2025-05-02 18:34:27 -0400

    Initial commit

---
+++ src/common/ResultUtils.java
@@ -0,0 +1,29 @@
+package common;
+
+public class ResultUtils {
+    /**
+     * 成功
+     *
+     */
+    public static <T> BaseResponse<T> success(T data) {
+        return new BaseResponse<>(0, data, "ok");
+    }
+
+
+    /**
+     * 失败
+     *
+     */
+    public static BaseResponse<?> error(ErrorCode errorCode) {
+        return new BaseResponse<>(errorCode);
+    }
+
+    /**
+     * 失败
+     *
+     */
+    public static BaseResponse<?> error(ErrorCode errorCode, String message) {
+        return new BaseResponse<>(errorCode.getCode(), null, message);
+    }
+
+}
